771bf6ed323f3da3406588b54c9bbd7a69b2342b
Richard Armijos <richardmijo@gmail.com>
2026-07-16 08:43:27 -0500

    Cambios para produccion

--- src/server.js
+++ src/server.js
@@ -49,6 +49,7 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor Express corriendo en el puerto ${PORT} y accesible desde la red local`);
+const HOST = process.env.HOST || '0.0.0.0';
+server.listen(PORT, HOST, () => {
+  console.log(`Servidor Express corriendo en http://${HOST}:${PORT}`);
 });
